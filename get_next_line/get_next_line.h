08e010af1f5e2850ce419b1e98789c9f781a10e9
YUE ZHANG <yaoruoli1989@gmail.com>
2025-05-29 17:03:40 +0200

    edit h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yzhang2 <yzhang2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 16:21:52 by yzhang2           #+#    #+#             */
-/*   Updated: 2025/05/29 15:23:05 by yzhang2          ###   ########.fr       */
+/*   Updated: 2025/05/29 17:03:10 by yzhang2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@
 #  define BUFFER_SIZE 24
 # endif
 
+char	*read_save(int fd, char *backup);
+char	*get_line(char *backup);
+char	*refresh_backup(char *backup);
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
